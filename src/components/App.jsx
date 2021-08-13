88f94b2eb939b3e1c259d36cc515091979308632
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-12 20:27:19 -0700

    add app files, src folder, installed bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,33 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "American Hardcore", author: "Stephen Blush"},
+            {title: "Fight Club", author: "Chuck Palanuik"},
+            {title: "Watership Down", author: "Richard Adams"}
+        ];
+        this.state = {
+            booknumber: 0
+        };
     }
 
         render() {
             return (
                 <div className="container-fluid">
                     <TitleBar /> 
+                        <div className="row">
+                        <div className="col-md-4">
+                        {/*Button to go to previous book goes here*/}
+                    </div>
+                    <div className="col-md-4">
+                        {/*Book will be displayed here*/}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4">
+                        {/*Button to go to next book goes here*/}
+                        </div>
+                    </div>
                 </div>
             )
     }
